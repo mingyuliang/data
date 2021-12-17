7d55b93c209fc96e848b1e567330fe553c07f98e
mingyuliang <92761806+mingyuliang@users.noreply.github.com>
2021-12-17 17:43:09 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+value = float (input('请输入成绩: '))
+if value > 90:
+    print('A')
+elif value >=80:
+    print('B')
+elif value >=70:
+    print('C')
+elif value >=60:
+    print('D')
+else:
+    print('E')
